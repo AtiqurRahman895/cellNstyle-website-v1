ac757295ee7cd3e068eff39fb3767cd91c4034d2
ebny-buniad <ebnybuniad@gmail.com>
2024-09-01 23:35:03 +0600

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,39 @@
+
+
+(function ($) {
+    "use strict"
+    $(window).on('load', function () {
+      $('#preloader-active').delay(1000).fadeOut('');
+      $('body').delay().css({
+        'overflow': 'visible'
+      });
+    });
+  
+  })(jQuery);
+
+
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('header').addClass("sticky");
+        }
+        else {
+            $('header').removeClass("sticky");
+        }
+    });
+    // copyrights Year Auto-update
+    function newDate() {
+        return new Date().getFullYear();
+    }
+    document.onload = document.getElementById("autodate").innerHTML = + newDate();
+
+});
+
+$(document).ready(function () {
+
+});
+
+
+
