b707096b5df313926a93de892471481510e01863
AtiqurRahman895 <emonhassan895@gmail.com>
2024-09-10 16:38:21 +0600

    ADDED PRODUCTS PAGES

--- asset/js/repair.js
+++ asset/js/repair.js
@@ -1,27 +1,5 @@
 let serviceReciveMethod =document.querySelectorAll(`.serviceReciveMethod`)
 
-serviceReciveMethod.forEach((eachServiceReciveMethod)=>{
-    let child=document.querySelector(`.${eachServiceReciveMethod.value}`)
-
-    if(eachServiceReciveMethod.checked){
-        child.classList.add(`d-flex`)
-        child.hidden=false;
-        child.querySelectorAll('input, button, select, textarea').forEach((el) => {
-            el.disabled = false;
-        });
-
-        // console.log(`${eachServiceReciveMethod.value}`)
-    }else{
-        child.classList.remove(`d-flex`)
-        child.hidden=true;
-        child.querySelectorAll('input, button, select, textarea').forEach((el) => {
-            el.disabled = true;
-        });
-        // console.log(`.${eachServiceReciveMethod.value}`)
-
-    }
-})
-
 let toogleMethods=()=>{
     serviceReciveMethod.forEach((eachServiceReciveMethod)=>{
         let child=document.querySelector(`.${eachServiceReciveMethod.value}`)
@@ -45,6 +23,8 @@ let toogleMethods=()=>{
         }
     })
 }
+toogleMethods();
+
 
 
 
